9f3a5f6a737602d2ee31baf51d5a993171a86c44
James Meyer <jcmeyer5@Jamess-MacBook-Pro.local>
2014-04-25 19:19:28 -0400

    Initial Commit

---
+++ parallax test/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  parallax test
+//
+//  Created by James Meyer on 4/25/14.
+//  Copyright James Meyer 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ parallax test/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  parallax test
+//
+//  Created by James Meyer on 4/25/14.
+//  Copyright James Meyer 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
